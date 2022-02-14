f82609578269fd561b8e04f534d21a9709da1110
morozvitali <morozvitali99@gmail.com>
2022-02-15 00:22:23 +0200

    Initial commit

---
+++ src/test/java/StackFifoTest.java
@@ -0,0 +1,23 @@
+import junit.framework.TestCase;
+
+public class StackFifoTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testPush() {
+    }
+
+    public void testPop() {
+    }
+
+    public void testIsArrayIsEmpty() {
+    }
+
+    public void testTestToString() {
+    }
+}
